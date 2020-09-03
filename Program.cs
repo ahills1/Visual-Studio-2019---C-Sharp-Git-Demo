27d28a4954f253d0137eaf66b45152080532210d
Ashley Hills <ahills1@usf.edu>
2020-09-03 15:01:35 -0400

    Commit the change

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Visual_Studio_2019___C_Sharp_Git_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
